f74729bb3ae98969ce15503c51a6ebcbcc0aed95
Ken Jiang <jiangk@bxscience.edu>
2022-03-26 12:24:00 -0400

    implement basic err handling

--- comm.rs
+++ comm.rs
@@ -1,12 +1,16 @@
 use std::env;
 
-fn main(){
-    let mut args = Vec::new()
-    args = env::args().collect()
-    
-    let mut file1 = String::new();
+fn comm() -> Result<String,String>{
+    let args : Vec<_> = env::args().collect();
+    if args.len() != 3{
+        return Err("comm needs two arguments to compare files!".to_string())
+    }
+    return Ok("Hello, World".to_string());
+}
 
-    io::stdin().read_line(&mut input)?;
-    println!("Hello, {}!", input.trim());
-    Ok(())
+fn main(){
+    match comm(){
+        Ok(result) => println!("{}", result),
+        Err(err) => println!("error: {}",err),
+    }
 }
