d9229c6a9ea56f2f5a7f86b92df44e3ed9956fbd
Ken Jiang <ken.jiang@mongodb.com>
2023-09-23 17:39:42 -0400

    compare-unlimited-files

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 use std::env;
 use std::fs::File;
 
-fn comm() -> Result<String,String>{
-    let args : Vec<String> = env::args().collect();
-    if args.len() != 3{
-        return Err("comm needs two arguments to compare files!".to_string())
+fn comm() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        return Err("comm needs at least two arguments to compare files!".to_string());
     }
-    let filename1 : &String = &args[1];
-    let filename2 : &String = &args[2];
 
-    let _file1 = File::open(filename1).expect("unable to open first file");
-    let _file2 = File::open(filename2).expect("unable to open second file");
-    return Ok("Hello, World".to_string());
+    let files: usize = args.len();
+    let mut files_opened = 0;
+
+    for i in 1..files {
+        let filename: &String = &args[i];
+        let file = File::open(filename);
+        match file {
+            Ok(_) => {
+                files_opened += 1;
+            }
+            Err(error) => {
+                println!("error opening file {}: {}", filename, error)
+            }
+        }
+    }
+
+    return Ok(format!("opened {} files", files_opened));
 }
 
-fn main(){
-    match comm(){
+fn main() {
+    match comm() {
         Ok(result) => println!("{}", result),
-        Err(err) => println!("error: {}",err),
+        Err(err) => println!("error: {}", err),
     }
 }
